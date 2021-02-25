074877199a23ce76bb0626e523899e8dc375547e
EthanVanwijkBDSC <78832211+EthanVanwijkBDSC@users.noreply.github.com>
2021-02-26 10:15:11 +1300

    Update pass manager.py

--- pass manager.py
+++ pass manager.py
@@ -1,54 +1,63 @@
 #defines vars
-
 chosen = 0
 name = ""
 age = 0
-pass_ = []
+pas = []
 users = []
 pass_inpt = ""
 i = 0
-
+user = ""
+usepas = []
+brec = "____________________________ \n"
 #intro
-print("welcome to password manager")
+print("--welcome to password manager--")
 
 #menu
 def menu(name, age):
-    print("hello " + name)
+    
+    print("welcome {}" .format(name))
+    
     if age < 13:
         print("your are to young to use the password manager!")
         print("goodbye")
         exit()
-    else:        
-        mode = int(input("1, 2, 3?"))
-        return mode
-#START OF user input
+
+#start of user input
+
 while True:
+    
     try:
         age = int(input("what is your age?: "))
-        all()
-    except ValueError:
-        print("invalid age") 
-    except:
-        print("an unknown error has ")
-
-def all():
-    name = input("what is your name?: ")
-    print(menu(name, age))
+        break
     
-    #main
-    while chosen == 1 or 2 or 0:
-        chosen = menu(name, age)
-        if chosen == 1:
-            pass_inpt = input("pass?")
-            pass_.append(pass_inpt)
-        elif chosen == 2:
-            for i in pass_:
-                print(pass_[i])
-                i += 1
-        
-        elif chosen == 3:
-            print("goodbye")
-            exit()
-        else:
-            print("this is not a eligible number")
-        main = 0
+    except ValueError:
+        print("this is an invalid number")  
+
+name = input("what is your name?: ")
+
+print(menu(name, age))
+    
+#main
+while chosen == 1 or 2 or 0:
+    chosen = int(input("-type 1 to add a password.\n-type 2 to view the lists.\n-type 3 to exit the program.\n: "))
+    
+    if chosen == 1:
+        print("")
+        site = input("what is this password used for?: ")
+        user = input("what is the username you want to add?: ")
+        pass_inpt = input("what is the password that goes with this site?: ")
+        usepas.append(" {} site: {} \n {} Username: {} \n {} Password: {} {}" .format(brec, site, brec, user, brec, pass_inpt, brec ))
+    
+    elif chosen == 2:
+        print(" ".join(usepas))
+    
+    elif chosen == 3:
+        print("goodbye")
+        exit()
+    
+    else:
+    
+        print("this is not a eligible number")
+    
+    main = 0
+    
